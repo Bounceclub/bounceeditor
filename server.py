f4a750c8793de421b8b856c997ddf2710deb0c83
Bounceclub <latamopium@gmail.com>
2026-04-26 09:15:33 -0300

    Update server to use environment variable for PORT

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import http.server
 import socketserver
+import os
 
-PORT = 10000
+PORT = int(os.environ.get("PORT", 8000))
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
     print("Serving at port", PORT)
     httpd.serve_forever()
